a877fe821a25c52b962fb09a7a4e4717382d7fb8
Camyl Magdalyn Tanjay <84168699+cmtanjay@users.noreply.github.com>
2023-12-02 05:29:11 +0800

    Image restoration button edits

--- img_restoration.py
+++ img_restoration.py
@@ -94,3 +94,57 @@ def minimum_filter(self):
         self.statusbar.grid(row=2, columnspan=3, sticky="ew")
         self.create_statusbar_canvas()
         self.add_text_to_statusbar(f"Status: Averaging filter is applied to the image on a {variables.n}x{variables.n} mask", x=220, y=20, fill="white", font=("Arial", 9,))
+
+
+def geometric_filter(self):
+    def get_pixel_value(neighbors):
+        # print("Neighbors:", neighbors)
+        
+        product = np.prod(neighbors)
+        
+        # Check if the product is non-positive
+        if product <= 0:
+            # Handle non-positive values, for example, return 0 or a default value
+            return 0
+        
+        epsilon = 1e-10  # Small positive constant to avoid numerical instability
+        return (product + epsilon)**(1.0 / len(neighbors))
+
+    if not variables.pcx_image_data:
+        print("No PCX Image Loaded")
+        self.add_text_to_statusbar("Status: No PCX image loaded", x=120, y=20, fill="white", font=("Arial", 9,))
+    else:
+        radius = variables.n // 2
+
+        if variables.isDegraded:
+            data = variables.degraded_image_data
+        else:
+            data = get_grayscale_img(self)
+
+        data_2D = [row[:] for row in data]
+        padded_img = clamp_padding(radius, data)
+
+        for i in range(variables.img_height):
+            for j in range(variables.img_width):
+                neighbors = get_neighbors(i + radius, j + radius, radius, padded_img)
+                data_2D[i][j] = get_pixel_value(neighbors)
+
+        geo_filtered_img = Image.new('L', (variables.img_width, variables.img_height), 255)
+        draw_geo_filtered = ImageDraw.Draw(geo_filtered_img)
+        drawImage(self, draw_geo_filtered, data_2D)
+
+        show_image(self, geo_filtered_img, " ")
+        variables.curr_image_data = data_2D
+        variables.curr_img = geo_filtered_img
+
+        self.statusbar.destroy()
+        self.statusbar = tk.Frame(self, height=30, bg="#2F333A", borderwidth=0.5, relief="groove")
+        self.statusbar.grid(row=2, columnspan=3, sticky="ew")
+        self.create_statusbar_canvas()
+        self.add_text_to_statusbar(f"Status: Geometric mean filter is applied to the image on a {variables.n}x{variables.n} mask", x=220, y=20, fill="white", font=("Arial", 9,))
+
+def contraharmonic_filter():
+    print("Contraharmonic filter ni hihi")
+
+def midpoint_filter():
+    print("Midpoint filter eme")
--- main.py
+++ main.py
@@ -385,26 +385,27 @@ class App(tk.Tk):
         self.opsbar = tk.Frame(self, width=100, bg="#313131")
         self.opsbar.grid(row=1, column=1, pady=(230, 90), sticky="nsew")
         
+        # Configures Geometric filter button
         img_geometric = Image.open("assets\gamma.png")
         img_geometric = img_geometric.resize((35,35))
         icon_geometric = ImageTk.PhotoImage(img_geometric)
         
-        # Configures Power-Law button
-        btn_geometric = tk.Button(self.opsbar, image=icon_geometric, command=lambda: salt_and_pepper_noise(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
+        btn_geometric = tk.Button(self.opsbar, image=icon_geometric, command=lambda: geometric_filter(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
         btn_geometric.photo = icon_geometric
         btn_geometric.grid(row=0, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_geometric, "Geometric Filter")
-        
+
+        # Configures Contraharmonic filter button
         img_contraharmonic = Image.open("assets\gamma.png")
         img_contraharmonic = img_contraharmonic.resize((35,35))
         icon_contraharmonic = ImageTk.PhotoImage(img_contraharmonic)
         
-        # Configures Power-Law button
-        btn_contraharmonic = tk.Button(self.opsbar, image=icon_contraharmonic, command=lambda: salt_and_pepper_noise(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
+        btn_contraharmonic = tk.Button(self.opsbar, image=icon_contraharmonic, command=lambda: contraharmonic_filter(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
         btn_contraharmonic.photo = icon_contraharmonic
         btn_contraharmonic.grid(row=1, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_contraharmonic, "Contraharmonic Filter")
         
+        # Configures Median Filter button
         img_mdn = Image.open("assets\median.png")
         img_mdn = img_mdn.resize((30,30))
         icon_mdn = ImageTk.PhotoImage(img_mdn)
@@ -414,32 +415,33 @@ class App(tk.Tk):
         btn_median.grid(row=2, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_median, "Median Filter")
         
+        # Configures Maximum filter button
         max_filter = Image.open("assets\gamma.png")
         max_filter = max_filter.resize((35,35))
         icon_max_filter = ImageTk.PhotoImage(max_filter)
         
-        # Configures Power-Law button
         btn_max_filter = tk.Button(self.opsbar, image=icon_max_filter, command=lambda: maximum_filter(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
         btn_max_filter.photo = icon_max_filter
         btn_max_filter.grid(row=3, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_max_filter, "Max Filter")
         
+        # Configures Minimum filter button
         min_filter = Image.open("assets\gamma.png")
         min_filter = min_filter.resize((35,35))
         icon_min_filter = ImageTk.PhotoImage(min_filter)
         
-        # Configures Power-Law button
         btn_min_filter = tk.Button(self.opsbar, image=icon_min_filter, command=lambda: minimum_filter(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
         btn_min_filter.photo = icon_min_filter
         btn_min_filter.grid(row=4, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_min_filter, "Min Filter")
-            
+
+        # Configures Midpoint filter button   
         mid_filter = Image.open("assets\gamma.png")
         mid_filter = mid_filter.resize((35,35))
         icon_mid_filter = ImageTk.PhotoImage(mid_filter)
         
-        # Configures Power-Law button
-        btn_mid_filter = tk.Button(self.opsbar, image=icon_mid_filter, command=lambda: salt_and_pepper_noise(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
+        
+        btn_mid_filter = tk.Button(self.opsbar, image=icon_mid_filter, command=lambda: midpoint_filter(self), background="#2F333A", foreground="white",relief="ridge", borderwidth=0)
         btn_mid_filter.photo = icon_mid_filter
         btn_mid_filter.grid(row=5, column=0, sticky="ew", padx=5, pady=10)
         CreateToolTip(btn_mid_filter, "Midpoint Filter")
