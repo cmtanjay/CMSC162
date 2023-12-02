0d8a79c67103b8572e6325953eae2c67e884f0b8
Camyl Magdalyn Tanjay <84168699+cmtanjay@users.noreply.github.com>
2023-12-02 14:46:58 +0800

    Initial contraharmonic filter

--- img_restoration.py
+++ img_restoration.py
@@ -1,3 +1,8 @@
+import numpy as np
+from PIL import Image, ImageDraw, ImageTk, ImageFilter
+import tkinter as tk
+from tkinter import ttk
+
 import variables
 from img_ops import *
 from img_enhancement import *
@@ -144,8 +149,95 @@ def geometric_filter(self):
         self.create_statusbar_canvas()
         self.add_text_to_statusbar(f"Status: Geometric mean filter is applied to the image on a {variables.n}x{variables.n} mask", x=220, y=20, fill="white", font=("Arial", 9,))
 
-def contraharmonic_filter():
-    print("Contraharmonic filter ni hihi")
+# def contraharmonic_mean(img, size, Q):
+#     """Smooth the given image with contraharmonic mean filter
+#        of given size and Q."""
+#     data = img_to_array(img, dtype=np.float64)
+#     numerator = np.power(data, Q + 1)
+#     denominator = np.power(data, Q)
+#     kernel = np.full(size, 1.0)
+#     result = filter2d(numerator, kernel) / filter2d(denominator, kernel)
+#     return array_to_img(result, img.mode)
 
-def midpoint_filter():
+def contraharmonic_filter(self):
+    print("Contraharmonic filter ni hihi")
+    def open_popup():
+        window = tk.Toplevel()
+        window.geometry("400x200+500+300")
+        window.title("Contraharmonic Filter")
+        window.resizable(False, False)
+
+        window.columnconfigure(0, weight=2)
+        window.columnconfigure(1, weight=5)
+
+        Q_var = tk.DoubleVar()
+
+        def entry_changed(event):
+            Q_value = Q_text_box.get()
+            try:
+                Q_value = float(Q_value)
+                Q_var.set(Q_value)
+            except ValueError:
+                pass
+
+        def on_click():
+            window.destroy()
+
+        ttk.Label(window, text='Input Q for Contraharmonic Filter:', font=('Arial 10 bold')).place(x=120, y=35)
+
+        Q_text_box = tk.Entry(window, bg="white", textvariable=Q_var, width=5, font=('Arial 13'))
+        Q_text_box.place(x=160, y=55)
+        Q_text_box.bind("<KeyRelease>", entry_changed)
+
+        btn = tk.Button(window, command=on_click, text="OK", font=("Arial", 10), background="#313E4E", foreground="white",
+                        relief="ridge", borderwidth=2)
+        btn.place(x=165, y=155)
+
+        window.wait_window(window)
+        return Q_var.get()
+
+    def get_pixel_value(neighbors, Q):
+        flat_neighbors = np.array(neighbors).flatten()
+        numerator = np.sum(np.power(flat_neighbors, Q + 1))
+        denominator = np.sum(np.power(flat_neighbors, Q))
+        return int(numerator / denominator) if denominator != 0 else 0
+
+    if not variables.pcx_image_data:
+        print("No PCX Image Loaded")
+        self.add_text_to_statusbar("Status: No PCX image loaded", x=120, y=20, fill="white", font=("Arial", 9,))
+    else:
+        Q = open_popup()
+
+        radius = variables.n // 2
+
+        if variables.isDegraded:
+            data = variables.degraded_image_data
+        else:
+            data = get_grayscale_img(self)
+
+        data_2D = [row[:] for row in data]
+        padded_img = clamp_padding(radius, data)
+
+        for i in range(variables.img_height):
+            for j in range(variables.img_width):
+                neighbors = get_neighbors(i + radius, j + radius, radius, padded_img)
+                data_2D[i][j] = get_pixel_value(neighbors, Q)
+
+        contraharmonic_img = Image.new('L', (variables.img_width, variables.img_height), 255)
+        draw_contraharmonic = ImageDraw.Draw(contraharmonic_img)
+        drawImage(self, draw_contraharmonic, data_2D)
+
+        show_image(self, contraharmonic_img, " ")
+        variables.curr_image_data = data_2D
+        variables.curr_img = contraharmonic_img
+
+        self.statusbar.destroy()
+        self.statusbar = tk.Frame(self, height=30, bg="#2F333A", borderwidth=0.5, relief="groove")
+        self.statusbar.grid(row=2, columnspan=3, sticky="ew")
+        self.create_statusbar_canvas()
+        self.add_text_to_statusbar(f"Status: Contraharmonic filter (Q={Q}) is applied to the image on a {variables.n}x{variables.n} mask",
+                                   x=220, y=20, fill="white", font=("Arial", 9,))
+        
+
+def midpoint_filter(self):
     print("Midpoint filter eme")
